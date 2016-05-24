6fa67d5cda28fb63b7799370607c0cbfbc683cb3
John Doe <johndoe@example.com>
2016-05-23 23:19:25 -0700

    v2

--- project4.cpp
+++ project4.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
+class City
+{
 
+	public:
+	
+		int cityIdentifier;
+	
+		int xCoordinate;
+		
+		int yCoordinate;
+		
+		int orderVisited;
+		
+		bool isVisited;
+		
+		
+
+};
+
+int displayCityVector(std::vector<City*> cityVector)
+{
+
+	for (std::vector<City*>::iterator iterator = cityVector.begin(); iterator != cityVector.end(); ++iterator)
+	{
+	
+		std::cout << "cityIdentifier: " << (*iterator)->cityIdentifier << " xCoordinate: " << (*iterator)->xCoordinate << " yCoordinate: " << (*iterator)->yCoordinate << " orderVisited: " << (*iterator)->orderVisited  << std::endl;
+		
+	
+	}
+
+	return 0;
+
+}
 
 int main(int argc, char** argv)
 {
@@ -13,14 +46,41 @@ int main(int argc, char** argv)
 	
 	int cityIdentifier, xCoordinate, yCoordinate;
 	
+	std::vector<City*> cityVector;
+	
+	City *currentCity;
+	
 	while (inputFile >> cityIdentifier >> xCoordinate >> yCoordinate)
 	{
 	
-		std::cout << cityIdentifier << " " << xCoordinate << " " << yCoordinate;
+		currentCity = new City;
+		
+		currentCity->cityIdentifier = cityIdentifier;
+		currentCity->xCoordinate = xCoordinate;
+		currentCity->yCoordinate = yCoordinate;
+		
+		cityVector.push_back(currentCity);
+		
+	
+		//std::cout << cityIdentifier << " " << xCoordinate << " " << yCoordinate;
+		
+		
+		
 	
 	}
 	
 	inputFile.close();
+	
+	displayCityVector(cityVector);
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	std::string outputFileName;
 	outputFileName = argv[1];
